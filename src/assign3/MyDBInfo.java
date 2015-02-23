df885953e439a0698fa7b161d04995729cfa3de4
Cleophus Robinson IV <CleoR41@gmail.com>
2015-02-22 21:38:35 -0800

    Uploading to server land

--- src/assign3/MyDBInfo.java
+++ src/assign3/MyDBInfo.java
@@ -8,8 +8,8 @@ package assign3;
  */
 public class MyDBInfo {
 	
-	public static final String MYSQL_USERNAME = "ccs108robinsca";
-	public static final String MYSQL_PASSWORD = "yaechood";
+	public static final String MYSQL_USERNAME = "";
+	public static final String MYSQL_PASSWORD = "";
 	public static final String MYSQL_DATABASE_SERVER = "mysql-user.stanford.edu";
 	public static final String MYSQL_DATABASE_NAME = "c_cs108_robinsca";
 
